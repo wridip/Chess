b1430b75ddbb43d8fd7d2721ee104be665292a52
wridip <wridip.sarkar@gmail.com>
2025-08-25 14:19:14 +0530

    updates

--- Chess/Chess .c
+++ Chess/Chess .c
@@ -95,15 +95,6 @@ void setup_board(char board[8][8])
     for (int j = 0; j < 8; j++) board[6][j] = 'P';
 }
 
-
-int main() 
-{
-    char board[8][8];
-    setup_board(board);
-    print_board(board);
-    return 0;
-}
-
 // Function to check if a rook can move from (row1, col1) to (row2, col2) on the chessboard
 // Returns 1 if valid move, 0 if blocked or illegal
 int check_rook_path(char board[8][8], int row1, int col1, int row2, int col2) 
@@ -252,21 +243,19 @@ int check_knight_path(char board[8][8], int row1, int col1, int row2, int col2)
         return 0; // Invalid move
 	}
 	//Knight can move in 8 possible ways
-    if ((col2 == col1 + 1 && row2 == row1 - 2) ||
-        (col2 == col1 + 2 && row2 == row1 - 1) ||
-        (col2 == col1 + 2 && row2 == row1 + 1) ||
-        (col2 == col1 + 1 && row2 == row1 + 2) ||
-        (col2 == col1 - 1 && row2 == row1 + 2) ||
-        (col2 == col1 - 2 && row2 == row1 + 1) ||
-        (col2 == col1 - 2 && row2 == row1 - 1) ||
-        (col2 == col1 - 1 && row2 == row1 - 2))
+    int valid = (col2 == col1 + 1 && row2 == row1 - 2) ||
+                (col2 == col1 + 2 && row2 == row1 - 1) ||
+                (col2 == col1 + 2 && row2 == row1 + 1) ||
+                (col2 == col1 + 1 && row2 == row1 + 2) ||
+                (col2 == col1 - 1 && row2 == row1 + 2) ||
+                (col2 == col1 - 2 && row2 == row1 + 1) ||
+                (col2 == col1 - 2 && row2 == row1 - 1) ||
+                (col2 == col1 - 1 && row2 == row1 - 2);
+	if (!valid)
     {
-        return 1; // Valid knight move
-    }
-    else
-    {
-        return 0; // Invalid knight move
-    }
+                return 0; // Invalid knight move
+	}
+
 
 	// Check if the destination square is occupied by own piece
     if (board[row2][col2] != ' ' && ((isupper(board[row1][col1]) && isupper(board[row2][col2])) || (islower(board[row1][col1]) && islower(board[row2][col2])))) 
@@ -280,12 +269,18 @@ int check_knight_path(char board[8][8], int row1, int col1, int row2, int col2)
 // Function to check if a queen can move from (row1, col1) to (row2, col2) on the chessboard
 int check_queen_path(char board[8][8], int row1, int col1, int row2, int col2)
 {
-    // A queen can move like a rook or a bishop
-    if (check_rook_path(board, row1, col1, row2, col2) || check_bishop_path(board, row1, col1, row2, col2))
+	if (row1 == row2 && col1 == col2) // Same square move is invalid
     {
-        return 1; // Valid queen move
+        return 0; // Invalid move
     }
-    return 0; // Invalid queen move
+
+    // A queen can move like a rook or a bishop
+    int valid = check_rook_path(board, row1, col1, row2, col2) || check_bishop_path(board, row1, col1, row2, col2);
+    
+    if (!valid)
+    {
+        return 0; // Invalid queen move
+	}
     /*
     This is an alternative way to check queen move without using rook and bishop functions
     if ((row1== row2 || col1 ==col2) || (abs(row2 - row1) == abs(col2 - col1)))
@@ -297,12 +292,6 @@ int check_queen_path(char board[8][8], int row1, int col1, int row2, int col2)
         printf("Invalid queen move\n");
     }
     */
-    //if same position -> invalid move
-
-    if (row1 == row2 && col1 == col2)
-    {
-        return 0; // Invalid move
-    }
 
     // Check if the destination square is occupied by own piece
     if (board[row2][col2] != ' ' && ((isupper(board[row1][col1]) && isupper(board[row2][col2])) || (islower(board[row1][col1]) && islower(board[row2][col2]))))
@@ -312,10 +301,106 @@ int check_queen_path(char board[8][8], int row1, int col1, int row2, int col2)
     return 1; // Valid queen move
 }
 // Function to check if a pawn can move from (row1, col1) to (row2, col2) on the chessboard
+// Function to check if a pawn move is valid
 int check_pawn_path(char board[8][8], int row1, int col1, int row2, int col2)
 {
-	char pawn = board[row1][col1]; // Determine if the pawn is white or black
-    // Pawn moves forward one square, or two squares from starting position
-	// Can only capture diagonally
-    
-    if 
+	char piece = board[row1][col1];
+
+	//Same square move is invalid
+    if (row1 == row2 && col1 == col2) 
+    {
+        return 0;
+	}
+	//White pawn moves upward (row decreases)
+    if (piece == 'P')
+    {
+		//Forward move by 1 (only if the square is empty)
+        if (col1 == col2 && row2 == row1 - 1 && board[row2][col2] == ' ')
+        {
+			return 1; // Valid move
+        }
+
+		//Forward move by 2 (only if starting row = 6 and both squares are empty)
+        if (col1 == col2 && row1 == 6 && row2 == 4 && board[5][col1] == ' ' && board[4][col1] == ' ')
+        {
+			return 1; // Valid move
+        }
+
+        //Diagonal capture by 1 (enemy must be black piece = lowecase)
+        if ((col2 == col1 + 1 || col2 == col1 - 1) && row2 == row1 - 1 && board[row2][col2] != ' ' && islower(board[row2][col2]))
+        {
+			return 1; // Valid capture
+        }
+    }
+
+    //Black pawn moves downward (row increases)
+    else if (piece == 'p')
+    {
+        //Forward move by 1 (only if the square is empty)
+        if (col1 == col2 && row2 == row1 + 1 && board[row2][col2] == ' ')
+        {
+            return 1; // Valid move
+        }
+
+        //Forward move by 2 (only if starting row = 1 and both squares are empty)
+        if (col1 == col2 && row1 == 1 && row2 == 3 && board[2][col1] == ' ' && board[3][col1] == ' ')
+        {
+            return 1; // Valid move
+        }
+
+        //Diagonal capture by 1 (enemy must be white piece = uppercase)
+        if ((col2 == col1 + 1 || col2 == col1 - 1) && row2 == row1 + 1 && board[row2][col2] != ' ' && isupper(board[row2][col2]))
+        {
+            return 1; // Valid capture
+        }
+    }
+
+	// If none of the valid moves matched, return 0
+	return 0; // Invalid pawn move
+}
+
+// forward declarations of your functions
+void setup_board(char board[8][8]);
+void print_board(char board[8][8]);
+int check_pawn_path(char board[8][8], int r1, int c1, int r2, int c2);
+int check_rook_path(char board[8][8], int r1, int c1, int r2, int c2);
+int check_knight_path(char board[8][8], int r1, int c1, int r2, int c2);
+int check_bishop_path(char board[8][8], int r1, int c1, int r2, int c2);
+int check_queen_path(char board[8][8], int r1, int c1, int r2, int c2);
+int check_king_path(char board[8][8], int r1, int c1, int r2, int c2);
+
+/*
+int main() {
+    char board[8][8];
+    setup_board(board);
+
+    while (1) {
+        print_board(board);
+
+        int r1, c1, r2, c2;
+        printf("Enter your move (from_row from_col to_row to_col): ");
+        scanf_s("%d %d %d %d", &r1, &c1, &r2, &c2);
+
+        char piece = board[r1][c1];
+        int valid = 0;
+
+        switch (tolower(piece)) {
+        case 'p': valid = check_pawn_path(board, r1, c1, r2, c2); break;
+        case 'r': valid = check_rook_path(board, r1, c1, r2, c2); break;
+        case 'n': valid = check_knight_path(board, r1, c1, r2, c2); break;
+        case 'b': valid = check_bishop_path(board, r1, c1, r2, c2); break;
+        case 'q': valid = check_queen_path(board, r1, c1, r2, c2); break;
+        case 'k': valid = check_king_path(board, r1, c1, r2, c2); break;
+        default: printf("No piece at that position!\n"); continue;
+        }
+
+        if (valid)
+            printf(" Valid move!\n\n");
+        else
+            printf(" Invalid move!\n\n");
+    }
+
+    return 0;
+}
+*/
+
